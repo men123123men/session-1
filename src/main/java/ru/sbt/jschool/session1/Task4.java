fdb138f618c0e11fdaee15043a8c80ff5078a0e9
men123123men <men123123men@yandex.ru>
2018-03-21 14:29:26 +0300

    добавил gitignore

--- src/main/java/ru/sbt/jschool/session1/Task4.java
+++ src/main/java/ru/sbt/jschool/session1/Task4.java
@@ -3,46 +3,52 @@ package ru.sbt.jschool.session1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Task4 {
-    public static void main(String[] args){
-        Pattern pattern = Pattern.compile("JSCHOOl1_COUNT=(?<COUNT>\\d+)");
-        Matcher matcher = pattern.matcher(args[0]);
+    public static void main(String[] args) throws IOException {
 
-        if (matcher.matches()) {
-            printHello(matcher.group("COUNT"));
+        String count;
+        String countPropertyName = "JSCHOOl1_COUNT";
+        String propertyFileNameInEnvironment = "JSCHOOL1_PROPERTIES_FILE";
+
+        Pattern pattern = Pattern.compile(countPropertyName+"=(?<COUNT>\\d+)");
+        Optional<Matcher> goodMatcher = Stream.of(args)
+                .map(pattern::matcher)
+                .filter(Matcher::matches)
+                .findFirst();
+
+        if(goodMatcher.isPresent())
+            count = goodMatcher.get().group("COUNT");
+
+        else if (System.getProperties().containsKey(countPropertyName))
+            count = System.getProperty(countPropertyName);
+
+        else if (System.getenv().containsKey(countPropertyName))
+            count = System.getenv(countPropertyName);
+
+        else if (System.getenv().containsKey(propertyFileNameInEnvironment)){
+            String fileName = System.getenv(propertyFileNameInEnvironment);
+            count = getValueFromPropertyFile(fileName, countPropertyName);
+
+        } else {
+            System.out.println("ничего не подошло, короче...");
             return;
         }
-        if (System.getProperties().containsKey("JSCHOOl1_COUNT")){
-            printHello(System.getProperty("JSCHOOl1_COUNT"));
-            return;
-        }
-        if (System.getenv().containsKey("JSCHOOl1_COUNT")){
-            printHello(System.getenv("JSCHOOl1_COUNT"));
-            return;
-        }
-        if (System.getenv().containsKey("JSCHOOL1_PROPERTIES_FILE")) {
-            String fileName = System.getenv("JSCHOOL1_PROPERTIES_FILE");
-            printHello(getCountFromFile(fileName));
-            return;
-        }
-        System.out.println("сообщение о вариантах исопльзования программы))");
+        printHello(count);
     }
     public static void printHello(String countOfHellowStr){
         int countOfHellowInt = Integer.parseInt(countOfHellowStr);
         for (int i = 0; i< countOfHellowInt;i++)
             System.out.println("Hello, World!");
     }
-    public static String getCountFromFile (String fileName) {
-        String[] fileLines = null;
-        try {
-            fileLines =  Files.lines(Paths.get(fileName)).toArray(String[]::new);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Плохой файл!!!!");
-        }
-        return fileLines[0];
+    public static String getValueFromPropertyFile (String fileName, String key) throws IOException {
+        Properties properties = new Properties();
+        properties.load(Files.newInputStream(Paths.get(fileName)));
+        return properties.getProperty(key);
     }
 }
