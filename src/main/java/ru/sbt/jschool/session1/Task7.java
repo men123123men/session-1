8ff942f090e9f7c003309903098cade6a45d5f51
men123123men <men123123men@yandex.ru>
2018-03-22 19:16:03 +0300

    внес комент в задачу с двумя массивами

--- src/main/java/ru/sbt/jschool/session1/Task7.java
+++ src/main/java/ru/sbt/jschool/session1/Task7.java
@@ -15,16 +15,46 @@ public class Task7 {
         System.out.println(Arrays.toString(arraysIntersection));
 
     }
+    /*
+    *
+    * Нужно ли сортировать второй массив?
+    * Да, нужно. Если мы боримся за вычислительную эффективность,
+    * то целесобразнее сначала отсортировать оба, а потом один раз пройтись в поисках соответствий!
+    *
+    *
+    * Какова сложность сортировки?
+    * Arrays.sort() -> DualPivotQuicksort.sort()
+    * А у быстрой сортировки сложность по времяни о(n*log(n)).
+    * (эта часть будет самая "тяжелая" по времяни во всем методе)
+    *
+    *
+    * Какова сложность поиска элемента в отсортированном массиве?
+    * Линейная(один раз проходимся по обоим массивам) по времени т.е. о(n)
+    *
+    *
+    * Что будет если 1 массив 1 000 000 элементов, а второй 2 элемента?
+    * Что будет если наоборот?
+    * В обоих случаях все отработает корректно за o(n*log(n)) по времяни.
+    *
+    */
+
     public static long[] intersection(long[] arr1, long[] arr2){
+        // o(n)
         long[]  first = arr1.clone();
+        // o(n)
         long[] second = arr2.clone();
 
+        // о(n*log(n))
         Arrays.sort(first);
+        // о(n*log(n))
         Arrays.sort(second);
 
+        // o(n) (JVM вынуждена пройтись по всему массиву и "забить" его нулями при создании)
         long[] intermediateArray = new long[first.length+second.length];
+        // o(1)
         int i=0, j=0, k=0;
 
+        // o(n)
         while (i<first.length && j<second.length) {
             if (first[i] == second[j]) {
                 intermediateArray[k] = first[i];
@@ -36,6 +66,9 @@ public class Task7 {
             } else
                 j++;
         }
+
+        // Arrays.copyOf() -> System.arraycopy() -> он "нативный"
+        // предполагаю что o(n)
         return Arrays.copyOf(intermediateArray,k);
     }
 }
